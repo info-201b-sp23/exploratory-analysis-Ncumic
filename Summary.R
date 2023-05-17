b4111d5466f4e4176f47f0bb74e0392ac73086de
“BonieXCW” <boniewang77701339@gmail.com>
2023-05-16 17:03:14 -0700

    read csv file directly from github

--- Summary.R
+++ Summary.R
@@ -2,10 +2,10 @@
 # DON'T FORGET TO RELABEL THEM WITH EASY TO READ TITLES(EX. SIG => SIGNIFICANCE)
 library("dplyr")
 # UPDATED TABLE CODE
-earthquake_data <- select(earthquake_data, -c("title"))
-
 
 earthquake_data <- read.csv('https://raw.githubusercontent.com/info-201b-sp23/exploratory-analysis-Ncumic/main/earthquake_data.csv')
+earthquake_data <- select(earthquake_data, -c("title"))
+
 
 
 # BONIE VIZ STUFF
