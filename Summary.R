257934dec93a934385aa67662c818b7c584722d0
“BonieXCW” <boniewang77701339@gmail.com>
2023-05-17 21:25:05 -0700

    Finished which country has the most accurate estimated of intensities

--- Summary.R
+++ Summary.R
@@ -1,13 +1,23 @@
 # CREATE NEW TABLE THAT HOLDS VALUES WE ARE USING(VALUES WE ARE USING ARE IN THE DISCORD)
 # DON'T FORGET TO RELABEL THEM WITH EASY TO READ TITLES(EX. SIG => SIGNIFICANCE)
 library("dplyr")
+library("tidyr")
 # UPDATED TABLE CODE
 
 earthquake_data <- read.csv('https://raw.githubusercontent.com/info-201b-sp23/exploratory-analysis-Ncumic/main/earthquake_data.csv')
-earthquake_data <- select(earthquake_data, -c("title","alert","net", "nst", "dmin", "gap", "magType", "depth"))
+earthquake_data_modified <- select(earthquake_data, -c("title","alert","net", "nst", "dmin", "gap", "magType", "depth"))
 
 
+#5) Which Country had the most accurate estimated of intensities?
+
+earthquake_data_Accuracy <- select(earthquake_data, c("magnitude", "mmi", "country"))
+earthquake_data_Accuracy <- earthquake_data_Accuracy[earthquake_data_Accuracy$country != "", , drop = FALSE]
+earthquake_data_Accuracy$accuracy <- (1 - abs(earthquake_data_Accuracy$mmi - earthquake_data_Accuracy$magnitude) / earthquake_data_Accuracy$magnitude) * 100
 
 
+CountryAccuracy <- earthquake_data_Accuracy %>%
+  group_by(country) %>%
+  summarize(avg_accuracy = mean(accuracy)) %>%
+  arrange(desc(avg_accuracy))
 
-
+CountryMostAccurate <- CountryAccuracy %>% head(1) %>% pull(country)
