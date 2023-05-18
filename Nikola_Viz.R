b54b3b4bdea671967f5ca1aebce234f1a4a6632f
Ncumic <Ncumic00@gmail.com>
2023-05-18 14:51:01 -0700

    finished world map for magnitude of earthquakes in Nikola_Viz file

--- Nikola_Viz.R
+++ Nikola_Viz.R
@@ -10,4 +10,17 @@ world_data_shape <- map_data("world")
 
 world_earthquake_table <- left_join(world_data_shape, earthquake_data_modified, by = c("long" = "longitude", "lat" = "latitude"))
 
+world_plot <- ggplot(data = world_data_shape) +
+  geom_polygon(aes(x = long,
+                   y = lat,
+                   group = group)) +
+  geom_point(data = earthquake_data_modified,
+             aes(x = longitude, y = latitude,
+             size = magnitude),
+             color = "Red",
+             # might change shape later
+             shape = 21
+  )
+
+
 
