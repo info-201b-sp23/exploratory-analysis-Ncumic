d643c888cdbd6efa72cd199e572eedf1e7736e3b
wbw2002 <xinying7@uw.edu>
2023-05-18 00:04:13 -0700

    Finished Christine‘s Chart

---
+++ Christine-chart.R
@@ -0,0 +1,38 @@
+# BB-5
+
+# load libraries
+library("dplyr")
+library("stringr")
+library("ggplot2")
+
+# load the data 
+# Download the SPL datasets and load here from a file path
+eq_df <- read.csv("https://raw.githubusercontent.com/info-201b-sp23/exploratory-analysis-Ncumic/main/earthquake_data.csv", 
+                  stringsAsFactors = FALSE)
+
+world_shape <- map_data("world")
+
+eq_mapping_df <- left_join(world_shape, eq_df, by = c("long" = "longitude", "lat" = "latitude"))
+
+world_plot <- ggplot(data = world_shape) +
+  geom_polygon(aes(x = long, 
+                   y = lat,
+                   group = group)) +
+  geom_point(data = filter(eq_df, tsunami == 0),
+             aes(x = longitude,
+                 y = latitude),
+             color = "blue") +
+  geom_point(data = filter(eq_df, tsunami == 1),
+             aes(x = longitude,
+                 y = latitude),
+             color = "red")
+world_plot
+
+#world_plot <- ggplot(data = world_shape) +
+#geom_polygon(aes(x = long, 
+#                 y = lat,
+#                 group = group)) +
+#  geom_point(data = eq_df,
+#             aes(x = longitude,
+#                 y = latitude),
+#             color = "blue")
