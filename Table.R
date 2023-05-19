dbbb688674bd2e5ea1fd057ebf1ae5ae2fba1db6
Ncumic <Ncumic00@gmail.com>
2023-05-18 21:24:17 -0700

    group by of data set

--- Table.R
+++ Table.R
@@ -7,8 +7,8 @@ earthquake_data <- read.csv('https://raw.githubusercontent.com/info-201b-sp23/ex
 
 earthquake_data_modified <- select(earthquake_data, -c("title","net", "nst", "dmin", "gap", "magType", "depth"))
 
-# Group_by certain aspects(probably location/country)
-
+# Group_by certain aspects(magnitude)
+earthquake_data_modified <- earthquake_data_modified %>% group_by(magnitude)
 # rename Columns inorder for better reading
 
 # Create the summary table
